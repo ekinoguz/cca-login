0ae39100cb59d2b9fac8c205963d70842b91958c
ekin oguz <ekinoguz@gmail.com>
2013-05-14 02:29:58 -0700

    Update cca.py

--- cca.py
+++ cca.py
@@ -3,7 +3,7 @@ Cisco Clean Access (CCA) login script if you do not want to lose your tabs when 
 
 Developed only for Ubuntu (which does not require NAC Agent) and tested only at UCI
 
-Usage: python cca-agent.py <UCInetID>
+Usage: python cca.py <UCInetID>
 
 This script is for educational purposes only, use it on your own risk.
 PS: for more info about CCA: http://en.wikipedia.org/wiki/Cisco_NAC_Appliance
